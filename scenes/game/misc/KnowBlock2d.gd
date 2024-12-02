b277db211af13f31e7856ef187ff4368f3f490b5
oiminion <arthurkoichi@gmail.com>
2024-12-02 14:54:21 -0300

    Apresentavel_1.2tantofaz

--- scenes/game/misc/KnowBlock2d.gd
+++ scenes/game/misc/KnowBlock2d.gd
@@ -30,6 +30,10 @@ func botaoPressionado(tituloG, tituloR, textoG,textoR):
 	$inGame.visible = 1 
 	$inRL.visible = 1
 	
+func adicoes(texto: String):
+	setTextGame("Adições futuras",texto)
+	$inGame.visible = 1 
+	
 func _on_ButtonRAM_pressed():
 	botaoPressionado(
 	"RAM no jogo",
@@ -96,3 +100,17 @@ func _on_KnowBButonProcessos_pressed():
 	"Existem vários estados de um processo, desde criando até encerrando. \n"+
 	"Um dos jeitos de multitarefa é compartilhar o tempo entre os processos, dando a impressão que o processador está fazendo multiplas tarefas ao mesmo tempo"
 	);
+
+
+func _on_AdicoesFuturas_pressed():
+	adicoes("Upgrades:\n" + 
+	"Upgrades serão uma progrmas/conceitos/harware que auxiliam na automação do sistema operacional exemplos são:\n" +
+	" - Os gerentes(escalonador de processos, gerente de arquivos e etc), programas que serão desbloqueados mais para o fim do jogo, onde automatizam o fluxo de dados\n" +
+	" - Quantum, será salvo uma configurção feita pelo jogador, de quanto tempo a CPU irá ficar conectada com cada processo e ao apertar um botão, essa configuração é realizada\n" +
+	" - Monoprogramado/multiprogramado será explicado com o desbloqueio de slots de processos(atualmente está começando com 5, mas provavelmente começará com 1 processo)\n" +
+	"\nTutorial:\n" +
+	"A BIOS será utilizada como tutorial do jogo, apresentando cada componete de hardware presente, sua função(tanto na vida real como em jogo) e ao terminar, passar o controle para o jogador\n" +
+	"\nSistema de salvamento:\n" + 
+	"Salvar somente o que está contido na memória secundária, para mostrar que a memória RAM e  a CPU são voláteis\n" +
+	"\nObservações:\n" +
+	"O jogo parece estar atrasado, porém já foram criados os módulos principais(por enquanto e tirando o salvamento), logo agora é questão de se aplicar esses módulos para fazer o resto\n")
